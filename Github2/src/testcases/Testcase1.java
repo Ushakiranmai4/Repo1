ae4f7d9c4c01f92828d756424b926fe928ff1550
Usha Kiranmai <rusha9846@gmail.com>
2023-04-12 10:44:58 +0530

    changed TestCase

--- Github2/src/testcases/Testcase1.java
+++ Github2/src/testcases/Testcase1.java
@@ -4,6 +4,10 @@ public class Testcase1 {
 	
 	public static void main(String[] args)
 	{
-		System.out.println("testcase added");	}
+		System.out.println("testcase added");
+
+		System.out.println("testcase is changed");
+		
+	}
 
 }
